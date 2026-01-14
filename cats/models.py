4373cfa9ff9d2321df91eeb6fd56c84ffb59c1b0
Кибакин Виктор <v.kibakin@yandex.ru>
2026-01-14 17:24:52 +0300

    add models for achievenets

--- cats/models.py
+++ cats/models.py
@@ -9,12 +9,28 @@ class Owner(models.Model):
         return f'{self.first_name} {self.last_name}'
 
 
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
         Owner, related_name='cats', on_delete=models.CASCADE)
+    achievement = models.ManyToManyField(Achievement, through='AchievementCat')
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'

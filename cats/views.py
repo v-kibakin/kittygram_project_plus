78713cb122af2e942b349df17314429c402c34b5
Кибакин Виктор <v.kibakin@yandex.ru>
2026-01-15 18:12:31 +0300

    add custom ViewSet-func

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Cat, Owner
 from .serializers import CatSerializer, OwnerSerializer
@@ -7,6 +9,14 @@ from .serializers import CatSerializer, OwnerSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='black')[:5]
+
+        serializer = self.get_serializer(cats, many=True)
+
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
